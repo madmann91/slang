fd40958aae44014e29f0c21f143b900987629344
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2016-10-14 16:22:22 +0200

    Stability checks in test suite

--- test/run.py
+++ test/run.py
@@ -6,13 +6,14 @@ import shutil
 
 passed = []
 failed = []
+instable = set()
 
-# launches a test on the given directory
+# Launches a test on the given directory
 def do_test(dir, opts):
     global passed
     global failed
     for f in os.listdir(dir):
-        if f.endswith(".glsl") :
+        if f.endswith(".glsl"):
             full_file = dir + "/" + f
             out_file = full_file + ".out"
             err_file = full_file + ".err"
@@ -35,6 +36,25 @@ def do_test(dir, opts):
                 shutil.copy(out_file, out_file + ".ref")
                 shutil.copy(err_file, err_file + ".ref")
 
+def check_stability(dir):
+    global passed
+    global instable
+    for f in passed:
+        if f.startswith(dir):
+            first  = open(f + "1", "w")
+            subprocess.call(["slangc", f], stdout = first)
+            first.close()
+
+            second = open(f + "2", "w")
+            subprocess.call(["slangc", f + "1"], stdout = second)
+            second.close()
+
+            if not filecmp.cmp(f + "1", f + "2"):
+                instable |= {f}
+            else:
+                os.remove(f + "1")
+                os.remove(f + "2")
+
 do_test("lexer/valid",          "--tokenize")
 do_test("lexer/invalid",        "--tokenize")
 do_test("preprocessor/invalid", "--preprocess")
@@ -42,9 +62,11 @@ do_test("preprocessor/valid",   "--preprocess")
 do_test("parser/invalid",       "--syntax")
 do_test("parser/valid",         "--syntax")
 
+check_stability("parser/valid")
+
 print("\nPassed :\n")
 for f in passed:
-    print("  " + f)
+    print("  " + f + (" [INSTABLE]" if f in instable else ""))
 
 print("\nFailed :\n")
 for f in failed:
